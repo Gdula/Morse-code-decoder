5168089615c2c8166c8717e3d93b022915ea5f8c
Gdula <da.gdula@gmail.com>
2019-10-16 00:49:14 +0200

    Implement morse decoding.

--- src/daniel/g/MorseCodeDecoder.java
+++ src/daniel/g/MorseCodeDecoder.java
@@ -5,39 +5,86 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MorseCodeDecoder {
-    public static void main(String[] args) {
+    private Map<String, String> morseMap;
 
+    public MorseCodeDecoder(Map<String, String> morseMap) {
+        this.morseMap = morseMap;
     }
-    public static String decode(String morseCode) {
-        StringBuilder decodedMessage = new StringBuilder();
-        Map<String, Character> morse = new HashMap<>();
-        morse.put("•-", 'a');
-        morse.put("—•••", 'b');
-        morse.put("—•—•", 'c');
-        morse.put("—••", 'd');
-        morse.put("•", 'e');
-        morse.put("••—•", 'f');
-        morse.put("——•", 'g');
-        morse.put("••••", 'h');
-        morse.put("••", 'i');
-        morse.put("•———", 'j');
-        morse.put("—•—", 'k');
-        morse.put("•—••", 'l');
-        morse.put("——", 'm');
-        morse.put("—•", 'n');
-        morse.put("———", 'o');
-        morse.put("•——•", 'p');
-        morse.put("——•—", 'q');
-        morse.put("•—•", 'r');
-        morse.put("•••", 's');
-        morse.put("—", 't');
-        morse.put("••—", 'u');
-        morse.put("•••—", 'v');
-        morse.put("•——", 'w');
-        morse.put("—••—", 'x');
-        morse.put("—•——", 'y');
-        morse.put("——••", 'z');
 
-        return null;
+    public Map<String, String> getMorseMap() {
+        return morseMap;
+    }
+
+    public void setMorseMap(Map<String, String> morseMap) {
+        morseMap.put(".-", "a");
+        morseMap.put("-...", "b");
+        morseMap.put("-.-.", "c");
+        morseMap.put("-..", "d");
+        morseMap.put(".", "e");
+        morseMap.put("..-.", "f");
+        morseMap.put("--.", "g");
+        morseMap.put("....", "h");
+        morseMap.put("..", "i");
+        morseMap.put(".---", "j");
+        morseMap.put("-.-", "k");
+        morseMap.put(".-..", "l");
+        morseMap.put("--", "m");
+        morseMap.put("-.", "n");
+        morseMap.put("---", "o");
+        morseMap.put(".--.", "p");
+        morseMap.put("--.-", "q");
+        morseMap.put(".-.", "r");
+        morseMap.put("...", "s");
+        morseMap.put("-", "t");
+        morseMap.put("..-", "u");
+        morseMap.put("...-", "v");
+        morseMap.put(".--", "w");
+        morseMap.put("-..-", "x");
+        morseMap.put("-.--", "y");
+        morseMap.put("--..", "z");
+        morseMap.put("-.-.--", "!");
+        morseMap.put(".-.-.-", ".");
+        morseMap.put("...---...", "SOS");
+        this.morseMap = morseMap;
+    }
+
+    public String decode(String messageToDecode) {
+        for (int i = 0; i < messageToDecode.length(); i++) {
+            if (messageToDecode.charAt(i) != '-' && messageToDecode.charAt(i) != '.' && messageToDecode.charAt(i) != ' ') {
+                messageToDecode = messageToDecode.replace(messageToDecode.charAt(i), ' ');
+            }
+        }
+        
+        messageToDecode = messageToDecode.trim();
+        messageToDecode += "  ";
+
+        StringBuilder decodedMessage = new StringBuilder();
+        StringBuilder morseWord = new StringBuilder();
+
+        for (int i = 0; i < messageToDecode.length(); i++) {
+
+            if (messageToDecode.charAt(i) != ' ') {
+                morseWord.append(messageToDecode.charAt(i));
+            }
+
+            if (messageToDecode.charAt(i) == ' ') {
+                decodedMessage.append(morseMap.get(morseWord.toString()));
+                morseWord = new StringBuilder();
+            }
+
+            if (messageToDecode.charAt(i) == ' ' && messageToDecode.charAt(i+1) == ' ' ) {
+                decodedMessage.append(' ');
+                i += 2;
+            }
+        }
+        return decodedMessage.toString().toUpperCase().trim();
+    }
+
+    public static void main(String[] args) {
+        Map<String, String> morseMap = new HashMap<>();
+        MorseCodeDecoder morseCodeDecoder = new MorseCodeDecoder(morseMap);
+        morseCodeDecoder.setMorseMap(morseMap);
+
+        System.out.println(morseCodeDecoder.decode("       9     ...---...      "));
     }
 }
